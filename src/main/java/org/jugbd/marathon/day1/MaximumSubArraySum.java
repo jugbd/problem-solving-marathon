10c3e44dc62d566ac37f4c1d9d2f29c0f31849bf
Bazlur Rahman <brahman@vantage.com>
2017-07-03 21:28:54 +0600

    Day #1 add maximum subarray sum problem
    The maximum sum subarray problem consists in finding the maximum
    sum of a contiguous subsequence in an array or list of integers.

---
+++ src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
@@ -0,0 +1,13 @@
+package org.jugbd.marathon.day1;
+
+/**
+ * @author Bazlur Rahman Rokon
+ * @since 7/3/17.
+ */
+public class MaximumSubArraySum {
+
+    public static int findMaxSum(int[] arr) {
+
+        return 4;
+    }
+}
---
+++ src/test/java/org/jugbd/marathon/day1/MaximumSubArraySumTest.java
@@ -0,0 +1,24 @@
+package org.jugbd.marathon.day1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Bazlur Rahman Rokon
+ * @since 7/3/17.
+ */
+public class MaximumSubArraySumTest {
+
+    @Test
+    public void testEmptyArray() throws Exception {
+        assertEquals("Empty arrays should have a max of 0", 0, MaximumSubArraySum.findMaxSum(new int[]{}));
+    }
+
+    @Test
+    public void testExampleArray() throws Exception {
+        assertEquals("Example array should have a max of 6",
+                6,
+                MaximumSubArraySum.findMaxSum(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+}
