75698fa143a763cb1b440264eb3de3468876101e
Md. Ashikuzzaman <ashikuzzaman.ar@gmail.com>
2017-07-03 22:37:17 +0600

    Updated MaximumSubArraySum.java class.
    Updated MaximumSubArraySum.java class for it's basic test case. It's just able to pass the basic test case already provided. No extra test case is added to test this class.

--- src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
+++ src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
@@ -2,12 +2,40 @@ package org.jugbd.marathon.day1;
 
 /**
  * @author Bazlur Rahman Rokon
+ * @author ashik
  * @since 7/3/17.
  */
 public class MaximumSubArraySum {
 
     public static int findMaxSum(int[] arr) {
 
-        return 4;
+        if (arr.length == 0) {
+
+            return 0;
+        } else {
+
+            int maxSum = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+
+                int tempSum = 0;
+                for (int j = i; j < arr.length; j++) {
+
+                    tempSum += arr[j];
+
+                    if (tempSum > maxSum) {
+
+                        maxSum = tempSum;
+                    }
+                }
+            }
+
+            if (maxSum > 0) {
+
+                return maxSum;
+            }
+
+            return 0;
+        }
     }
 }
