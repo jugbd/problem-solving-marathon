19aa2e298245cc14a6eb448e97b5374f03f9c2c5
gilfoyle007 <thejavalover007@gmail.com>
2017-07-03 23:34:04 +0600

    Update MaximumSubArraySum.java

--- src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
+++ src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
@@ -7,7 +7,17 @@ package org.jugbd.marathon.day1;
 public class MaximumSubArraySum {
 
     public static int findMaxSum(int[] arr) {
+        int max_sum = 0;
+        int current = 0;
 
-        return 4;
+        for (int i = 0; i < arr.length; i++)
+        {
+            current = current + arr[i];
+            if (max_sum < current)
+                max_sum = current;
+            if (current < 0)
+                current = 0;
+        }
+        return max_sum;
     }
 }
