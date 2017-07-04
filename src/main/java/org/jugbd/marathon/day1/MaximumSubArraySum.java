7021168151c31f6bbade522f8a2e7ae227285e55
Mukit <Mukit>
2017-07-04 17:50:07 +0600

    Java File Has Been Updated

--- src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
+++ src/main/java/org/jugbd/marathon/day1/MaximumSubArraySum.java
@@ -8,6 +8,23 @@ public class MaximumSubArraySum {
 
     public static int findMaxSum(int[] arr) {
 
-        return 4;
+        int max=0, v;
+    	int length=arr.length;
+    	int save[]=new int[arr.length];
+    	
+    	for(int i=0;i<length;i++) {
+    		
+    		save[i]=i>0?(save[i-1]+arr[i]):arr[i];
+    	}
+    //	-2 1 -3 4 -1 2 1 -5 4
+    	for(int i=0;i<length;i++) {
+    		
+    		for(int j=i+1;j<length;j++) {
+    				
+    			v=i>0?(save[j]-save[i-1]):save[j];
+    			max=Math.max(max, v);
+    		}
+    	}
+        return max;
     }
 }
