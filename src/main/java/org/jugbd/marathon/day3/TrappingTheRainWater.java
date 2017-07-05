a6136e6826958e81dc42e043b6805186900cc891
Bazlur Rahman <brahman@vantage.com>
2017-07-05 20:30:14 +0600

    Add Day#3 - Trapping Rain Water
    Given n non-negative integers representing an elevation map where
    the width of each bar is 1, compute how much water it is able to
    trap after raining.

---
+++ src/main/java/org/jugbd/marathon/day3/TrappingTheRainWater.java
@@ -0,0 +1,12 @@
+package org.jugbd.marathon.day3;
+
+
+public class TrappingTheRainWater {
+    static int findWaterUnit(int[] arr) {
+        int unitOfWater = 0;
+
+        // your awesome and mind bending solution goes here
+        return unitOfWater;
+    }
+
+}
---
+++ src/test/java/org/jugbd/marathon/day3/TrappingTheRainWaterTest.java
@@ -0,0 +1,16 @@
+package org.jugbd.marathon.day3;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrappingTheRainWaterTest {
+
+    @Test
+    public void basicTest() {
+        assertEquals(2, TrappingTheRainWater.findWaterUnit(new int[]{2, 0, 2}));
+        assertEquals(10, TrappingTheRainWater.findWaterUnit(new int[]{3, 0, 0, 2, 0, 4}));
+        assertEquals(6, TrappingTheRainWater.findWaterUnit(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+}
