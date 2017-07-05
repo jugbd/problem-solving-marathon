3b5783c586624ff85aeaf8eb017b664d3d7295cf
Mukit <Mukit>
2017-07-05 13:00:11 +0600

    Test Case Added & Java File Changed For BiggestNumber Problem

--- src/main/java/org/jugbd/marathon/day1/BiggestNumber.java
+++ src/main/java/org/jugbd/marathon/day1/BiggestNumber.java
@@ -1,10 +1,44 @@
 package org.jugbd.marathon.day1;
 
-
 public class BiggestNumber {
-    public static long findTheNextBiggerNumber(long n) {
-        //write your awesome code here
+	public static long findTheNextBiggerNumber(long n) {
 
-        return n;
-    }
+		int ar[]=new int[20]; // long is 64 bit, so max 19 digits can be possible
+		int length=0;
+		boolean found=false;
+		while(n>0) {
+
+			ar[length++]=(int) (n%10);
+			n/=10;
+		}
+
+		for(int i=0;i<length;i++) {
+
+			for(int j=i+1;j<length;j++) {
+
+				if(ar[j] < ar[i]) {
+
+					int tmp=ar[i];
+					for(int k=i+1;k<=j;k++) {
+
+						ar[k-1]=ar[k];
+					}
+
+					ar[j]=tmp;
+					found=true;
+					break;
+				}
+			}
+			if(found)
+				break;
+		}
+
+		n=0;
+		for(int i=length-1;i>=0;i--) {
+
+			n=n*10+ar[i];
+		}
+
+		return found?n:-1;
+	}
 }
