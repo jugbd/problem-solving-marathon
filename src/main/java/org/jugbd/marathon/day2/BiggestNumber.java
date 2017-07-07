a14dcde60735e7e11f3f7799fca93f20eba2ed06
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2017-07-08 00:42:21 +0600

    Format code

--- src/main/java/org/jugbd/marathon/day2/BiggestNumber.java
+++ src/main/java/org/jugbd/marathon/day2/BiggestNumber.java
@@ -1,50 +1,50 @@
 package org.jugbd.marathon.day2;
 
-public class BiggestNumber {
-	public static long findTheNextBiggerNumber(long n) {
+import java.util.Arrays;
 
-		int ar[]=new int[20]; // long is 64 bit, so max 19 digits can be possible
-		int length=0,tmp;
-		boolean found=false;
-		while(n>0) {
-			
-			ar[length++]=(int) (n%10);
-			n/=10;
-		}
-		
-		for(int i=0, j=length-1;i<length/2;i++,j--) {
-			
-			tmp=ar[i];
-			ar[i]=ar[j];
-			ar[j]=tmp;
-		}
-			
-		// 52876
-		
-		for(int i=length-1;i>=0;i--) {
-		
-			for(int j=i-1;j>=0;j--) {
-				
-				if(ar[j] < ar[i]) {
-					
-					tmp=ar[i];
-					ar[i]=ar[j];
-					ar[j]=tmp;
-					Arrays.sort(ar, j+1, length);
-					found=true;
-					break;
-				}
-			}
-			if(found)
-				break;
-		}
-		
-		n=0;
-		for(int i=0;i<length;i++) {
-			
-			n=n*10+ar[i];
-		}
-		
-		return found?n:-1;
-	}
+public class BiggestNumber {
+    public static long findTheNextBiggerNumber(long n) {
+
+        int ar[] = new int[20]; // long is 64 bit, so max 19 digits can be possible
+        int length = 0, tmp;
+        boolean found = false;
+        while (n > 0) {
+
+            ar[length++] = (int) (n % 10);
+            n /= 10;
+        }
+
+        for (int i = 0, j = length - 1; i < length / 2; i++, j--) {
+            tmp = ar[i];
+            ar[i] = ar[j];
+            ar[j] = tmp;
+        }
+
+        // 52876
+
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (ar[j] < ar[i]) {
+
+                    tmp = ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = tmp;
+                    Arrays.sort(ar, j + 1, length);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        n = 0;
+        for (int i = 0; i < length; i++) {
+            n = n * 10 + ar[i];
+        }
+
+        return found ? n : -1;
+    }
 }
