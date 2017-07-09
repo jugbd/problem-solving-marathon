6b6872b86daa16f9610250ada1e34917176aa64b
Mukit <Mukit>
2017-07-09 16:13:32 +0600

    Directory.java modified

--- src/main/java/org/jugbd/marathon/day4/Directory.java
+++ src/main/java/org/jugbd/marathon/day4/Directory.java
@@ -1,29 +1,61 @@
 package org.jugbd.marathon.day4;
 
 import java.util.Date;
+import java.util.ArrayList;
 
 public class Directory {
-    private String name;
+	
+	private String name;
     private Date createdDate;
+    private ArrayList<Directory> directoryList; 
+    private ArrayList<File> fileList; 
 
     public Directory(String root) {
+    	
+    	this.name = root;
+    	this.createdDate = new Date();
+    	this.directoryList = new ArrayList<>();
+    	this.fileList = new ArrayList<>();
+    }
+    
+    public void add(Directory directory) {
 
+    	this.directoryList.add(directory);
     }
 
     public void add(File file) {
 
+    	this.fileList.add(file);
+    }
+    
+    public int getAllFileCounts(Directory dir) {
+    	
+    	if(dir.directoryList.size() == 0)
+    		return dir.fileList.size();
+    	
+    	int length = dir.directoryList.size();
+    	int sumOfFiles = 0;
+    	
+    	for(int i = 0; i < length; i++) {
+    		
+    		sumOfFiles += getAllFileCounts(dir.directoryList.get(i));
+    	}
+    	return sumOfFiles + dir.fileList.size();
     }
 
     public int numberOfFiles() {
 
-        return 0;
+        return getAllFileCounts(this);
     }
 
-    public Object getName() {
+    public String getName() {
+    	
         return name;
     }
 
     public Date getCreatedDate() {
+    	
         return createdDate;
     }
+
 }
