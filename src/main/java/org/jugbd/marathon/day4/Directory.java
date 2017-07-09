fa3071680fa6e9615901da3613e223280f642522
ParthoShuvo <shuvojitsahashuvo@gmail.com>
2017-07-09 23:56:15 +0600

    Day4 solution has been added

--- src/main/java/org/jugbd/marathon/day4/Directory.java
+++ src/main/java/org/jugbd/marathon/day4/Directory.java
@@ -1,29 +1,32 @@
 package org.jugbd.marathon.day4;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Directory {
-    private String name;
-    private Date createdDate;
+	private String name;
+	private Date createdDate;
+	private List<File> files = new ArrayList<>();
 
-    public Directory(String root) {
+	public Directory(String root) {
+		this.name = root;
+		this.createdDate = new Date();
+	}
 
-    }
+	public void add(File file) {
+		files.add(file);
+	}
 
-    public void add(File file) {
+	public int numberOfFiles() {
+		return files.size();
+	}
 
-    }
+	public Object getName() {
+		return name;
+	}
 
-    public int numberOfFiles() {
-
-        return 0;
-    }
-
-    public Object getName() {
-        return name;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
+	public Date getCreatedDate() {
+		return createdDate;
+	}
 }
--- src/main/java/org/jugbd/marathon/day4/File.java
+++ src/main/java/org/jugbd/marathon/day4/File.java
@@ -1,14 +1,13 @@
 package org.jugbd.marathon.day4;
 
-
 public class File {
-    private String name;
+	private String name;
 
-    public File(String name) {
+	public File(String name) {
+		this.name = name;
+	}
 
-    }
-
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 }
